2114bd5cfc976e7251154dce2b551b0627b4a12f
artur <zeroone02@mail.ru>
2023-06-24 16:36:42 +0300

    Add project files.

---
+++ MyLeetCode/Array/L1909.cs
@@ -0,0 +1,28 @@
+﻿namespace MyLeetCode.Array;
+public class L1909
+{
+    public bool CanBeIncreasing(int[] nums)
+    {
+        int count = 0;
+        int value = nums[0];
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > value)
+            {
+                value = nums[i];
+                i++;
+            }
+            else
+            {
+                count++;
+                if (count > 2)
+                {
+                    return false;
+                }
+                value = nums[i - 1];
+            }
+        }
+        return true;
+    }
+}
+
