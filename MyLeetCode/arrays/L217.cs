ece1e0ce7560f5e30bd4170eb9bb4039e3923681
artur <zeroone02@mail.ru>
2023-06-28 19:17:12 +0300

    add array problems

---
+++ MyLeetCode/arrays/L217.cs
@@ -0,0 +1,13 @@
+﻿namespace MyLeetCode.arrays;
+public class L217
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        HashSet<int> visited = new HashSet<int>(nums);
+        if (nums.Length == visited.Count)
+        {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ MyLeetCode/arrays/L242.cs
@@ -0,0 +1,20 @@
+﻿namespace MyLeetCode.arrays;
+public class L242
+{
+    public bool IsAnagram(string s, string t)
+    {
+        char[] chars = s.ToCharArray();
+        Array.Sort(chars);
+        string sortedStr = new string(chars);
+
+        char[] chars2 = t.ToCharArray();
+        Array.Sort(chars2);
+        string sortedStr2 = new string(chars2);
+
+        if (sortedStr != sortedStr2)
+        {
+            return false;
+        }
+        return true;
+    }
+}
