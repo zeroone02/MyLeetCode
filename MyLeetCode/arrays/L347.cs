12157d76ea298ace48b9909c4b4bdd8fb895c5fb
Nikita <shinigamichan@yandex.ru>
2023-06-29 16:47:35 +0300

    add new leetcode problems

---
+++ MyLeetCode/arrays/L347.cs
@@ -0,0 +1,22 @@
+﻿namespace MyLeetCode.arrays;
+public class L347
+{
+    static int[] TopKFrequent(int[] nums, int k)
+    {
+        var dict = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!dict.ContainsKey(nums[i]))
+            {
+                dict.Add(nums[i], 1);
+            }
+            else
+            {
+                dict[nums[i]]++;
+            }
+        }
+        var ordered = dict.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+        int[] res = ordered.Select(x => x.Key).Take(k).ToArray();
+        return res;
+    }
+}
